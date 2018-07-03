5e40f3baf49f50fedb02095ce6d159d87089ddf1
windymsth <windymsth@users.noreply.github.com>
2018-07-03 22:01:18 +0800

    0703

--- GPS_Parsing.ino
+++ GPS_Parsing.ino
@@ -147,3 +147,89 @@ void GPS_Parsing(void) {
 //    }
   }
 }
+
+
+
+void task_saftyHandle(void) {
+  
+}
+
+void task_balloon_separation(void) {
+  task_saftyHandle();
+  //
+  if( GPS.fix == true && GPS.fixquality == true && F_takeoff == false) {
+    if( F_GetAlt == false ) {
+      takeoff_gps_alt = sys_data.gps_altitude;
+      takeoff_brao_alt = sys_data.baro_altitude;
+      RF_SERIAL.println("Get nowtime ground GPS_Altitude and Baro_Altitude !");
+      RF_SERIAL.print("Ground GPS_Altitude: ");RF_SERIAL.println(takeoff_gps_alt);  
+      RF_SERIAL.print("Ground Baro_Altitude: ");RF_SERIAL.println(takeoff_brao_alt);
+      F_GetAlt = true;
+    }
+    if( ((sys_data.gps_altitude - takeoff_gps_alt) > 50 || (sys_data.baro_altitude - takeoff_brao_alt) > 50) 
+        && F_takeoff == false) {
+      RF_SERIAL.println("ATTENTION ! SPACEBALLON TAKEOFF !");
+      RF_SERIAL.println("ATTENTION ! SPACEBALLON TAKEOFF !");
+      RF_SERIAL.println("ATTENTION ! SPACEBALLON TAKEOFF !");
+      F_takeoff = true;
+      takeoff_monment_time_sec = millis() / 1000;
+    }
+  }
+
+  // 进入飞行上升阶段执行程序
+  if(F_takeoff == true) {
+    static volatile long nowtime = 0;
+    static volatile long lasttime = 0;
+    static volatile long offset_baroAlt = 0;
+      nowtime = millis();
+      // 飞行阶段一：飞行上升到海拔9000米高度，校准气压高度偏差值；每10ms检查一次
+      if( nowtime - lasttime >= 10 && F_9000m_Baro_getOffset == false ) { 
+        lasttime = nowtime;
+
+        // 当GPS海拔高度到达9000，并且还没有补偿气压高度。同时距离释放时间超过30秒
+        if( sys_data.gps_altitude >= 9000 && (nowtime - takeoff_monment_time_sec) >= 30 ) {
+          offset_baroAlt = sys_data.gps_altitude - sys_data.baro_altitude;
+          Safty_9000m_timer = millis();
+          F_9000m_Baro_getOffset = true;
+          RF_SERIAL.println("ATTENTION ! SPACEBALLON RISE TO 9000m !");
+        }
+      }
+      
+    //飞行阶段二：继续上升到海拔12000米高度，准备释放动作
+    if( F_9000m_Baro_getOffset ==true ) {
+      
+      //  Safty_9000m_timer：9000m时启动的安全计时器
+      if( (millis() - Safty_9000m_timer) > 10*1000 ) {
+          RF_SERIAL.println("WARNING ! SAFTY_TIMER_A ACTION !");
+          
+          Action_separation();
+
+          RF_SERIAL.println("ATTENTION ! SEPARATION ACTION !");
+      }
+      
+      //  释放策略A：如果GPS可靠，依赖GPS高程执行释放
+      if( sys_data.gps_altitude > 11000 ) {
+        
+        if( sys_data.gps_altitude > 12000 ) {
+          
+          Action_separation();
+          
+          RF_SERIAL.println("ATTENTION ! SEPARATION ACTION !");
+        }
+      }
+      
+      //  释放策略B：如果GPS不可靠，依赖BARO高程执行释放
+      else {
+
+        if( sys_data.baro_altitude > 12000 ) {
+          
+          Action_separation();
+          
+          RF_SERIAL.println("ATTENTION ! SEPARATION ACTION !");
+        }
+      }
+    }// *** end ->  "if( F_9000m_Baro_getOffset ==true )"
+
+  }// *** end ->  "if(F_takeoff == true)"
+}
+
--- cubesat_spaceballon.ino
+++ cubesat_spaceballon.ino
@@ -6,9 +6,14 @@
 //#define PROCESSING_SERIAL_DEBUG   true
 //#define PROCESSING_SERIAL_DEBUG   flase
 /*-------Ver Config--------------*/
+
+uint8_t sliceType = pm2_5Type;
+volatile uint32_t timeSlice = 0;
+volatile uint32_t nowTimes = 0;
+
 void setup()
 {
-  Serial.begin(250000);
+  Serial.begin(115200);
   Serial.println("Serial Debug Port Init OK!");
   Wire.begin();
   Serial.println("IIC Bus Enable...\r\n");
@@ -19,7 +24,7 @@ void setup()
 	SHT31_Init();
 	Compass_hmc5883l_Init();
   BMP180_Init();
-  UHF_Init(9600);
+  UHF_Init(115200);
   SDlogger_init();
   Serial.println("Periheral Init finished!");Serial.println();
   timeSlice = millis();
@@ -32,91 +37,7 @@ volatile float takeoff_gps_alt = 0.0;
 volatile float takeoff_brao_alt = 0.0;
 volatile long takeoff_monment_time_sec = 0;
 volatile long Safty_9000m_timer = 0;
-void task_saftyHandle(void) {
-  
-}
-void task_balloon_separation(void) {
-  task_saftyHandle();
-  //
-  if( GPS.fix == true && GPS.fixquality == true && F_takeoff == false) {
-    if( F_GetAlt == false ) {
-      takeoff_gps_alt = sys_data.gps_altitude;
-      takeoff_brao_alt = sys_data.baro_altitude;
-      RF_SERIAL.println("Get nowtime ground GPS_Altitude and Baro_Altitude !");
-      RF_SERIAL.print("Ground GPS_Altitude: ");RF_SERIAL.println(takeoff_gps_alt);  
-      RF_SERIAL.print("Ground Baro_Altitude: ");RF_SERIAL.println(takeoff_brao_alt);
-      F_GetAlt = true;
-    }
-    if( ((sys_data.gps_altitude - takeoff_gps_alt) > 50 || (sys_data.baro_altitude - takeoff_brao_alt) > 50) 
-        && F_takeoff == false) {
-      RF_SERIAL.println("ATTENTION ! SPACEBALLON TAKEOFF !");
-      RF_SERIAL.println("ATTENTION ! SPACEBALLON TAKEOFF !");
-      RF_SERIAL.println("ATTENTION ! SPACEBALLON TAKEOFF !");
-      F_takeoff = true;
-      takeoff_monment_time_sec = millis() / 1000;
-    }
-  }
 
-  // 进入飞行上升阶段执行程序
-  if(F_takeoff == true) {
-    static volatile long nowtime = 0;
-    static volatile long lasttime = 0;
-    static volatile long offset_baroAlt = 0;
-      nowtime = millis();
-      // 飞行阶段一：飞行上升到海拔9000米高度，校准气压高度偏差值；每10ms检查一次
-      if( nowtime - lasttime >= 10 && F_9000m_Baro_getOffset == false ) { 
-        lasttime = nowtime;
-
-        // 当GPS海拔高度到达9000，并且还没有补偿气压高度。同时距离释放时间超过30秒
-        if( sys_data.gps_altitude >= 9000 && (nowtime - takeoff_monment_time_sec) >= 30 ) {
-          offset_baroAlt = sys_data.gps_altitude - sys_data.baro_altitude;
-          Safty_9000m_timer = millis();
-          F_9000m_Baro_getOffset = true;
-          RF_SERIAL.println("ATTENTION ! SPACEBALLON RISE TO 9000m !");
-        }
-      }
-      
-    //飞行阶段二：继续上升到海拔12000米高度，准备释放动作
-    if( F_9000m_Baro_getOffset ==true ) {
-      
-      //  Safty_9000m_timer：9000m时启动的安全计时器
-      if( (millis() - Safty_9000m_timer) > 10*1000 ) {
-          RF_SERIAL.println("WARNING ! SAFTY_TIMER_A ACTION !");
-          
-          Action_separation();
-
-          RF_SERIAL.println("ATTENTION ! SEPARATION ACTION !");
-      }
-      
-      //  释放策略A：如果GPS可靠，依赖GPS高程执行释放
-      if( sys_data.gps_altitude > 11000 ) {
-        
-        if( sys_data.gps_altitude > 12000 ) {
-          
-          Action_separation();
-          
-          RF_SERIAL.println("ATTENTION ! SEPARATION ACTION !");
-        }
-      }
-      
-      //  释放策略B：如果GPS不可靠，依赖BARO高程执行释放
-      else {
-
-        if( sys_data.baro_altitude > 12000 ) {
-          
-          Action_separation();
-          
-          RF_SERIAL.println("ATTENTION ! SEPARATION ACTION !");
-        }
-      }
-    }// *** end ->  "if( F_9000m_Baro_getOffset ==true )"
-
-  }// *** end ->  "if(F_takeoff == true)"
-}
-
-uint8_t sliceType = pm2_5Type;
-volatile uint32_t timeSlice = 0;
-volatile uint32_t nowTimes = 0;
 void loop()
 {
   static uint8_t task_comm = 0;
